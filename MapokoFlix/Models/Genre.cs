4495141215cb47495f45e8f95ff665f137e19257
MatheusRisatti <risattimatheus@gmail.com>
2023-05-15 09:39:19 -0300

    alterados Genre e Movie

--- MapokoFlix/Models/Genre.cs
+++ MapokoFlix/Models/Genre.cs
@@ -1,6 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MapokoFlix.Models;
     public class Genre
     {
+        [Key]
         public byte Id { get; set; }
+
+        [Required]
+        [StringLength(30)]
         public string Name { get; set; }
     }
